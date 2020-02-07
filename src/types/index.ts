e13a635f31426f7d8d4f3576c9c64bdb8323a9d9
xutongsen <501779683@qq.com>
2020-02-07 17:23:35 +0800

    feat(url process):

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequest {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
